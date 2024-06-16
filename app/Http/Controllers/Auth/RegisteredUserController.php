f53fc9eab0e449ccbdd4924d42d2497d4f2b88c5
Nikoloz Shervashidze <51024662+nsher888@users.noreply.github.com>
2024-06-16 17:39:50 +0400

    project setup

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): Response
+    {
+        $request->validate([
+            'name' => ['required'],
+            'email' => ['required'],
+            'password' => ['required'],
+            'idNumber' => ['required'],
+            'dateOfBirth' => ['required'],
+            'surname' => ['required'],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->string('password')),
+            'idNumber' => $request->idNumber,
+            'dateOfBirth' => $request->dateOfBirth,
+            'surname' => $request->surname,
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return response()->noContent();
+    }
+}
